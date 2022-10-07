d127106547adc24b255a461a466dba9e3223275d
Klaster28 <89096914521@mail.ru>
2022-10-07 18:21:18 +0300

    Коммит задание 1.2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+// Задача 1.1
+        int contributionRob = 15000;
+        int totAl = 0;
+        for (int q = 0; totAl < 2459000; q++) {
+            totAl = totAl + contributionRob;
+            totAl = totAl + totAl / 100;
+            System.out.println("Месяц " + q + " сумма накопления равна " + totAl);
+            System.out.println(totAl);
+            System.out.println(q);
+        }
+// Задача 1.2
+        int a = 1;
+        while (a <= 10) {
+            System.out.println (a + " ");
+            a++;
+        }
 
 
     }
